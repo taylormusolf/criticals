ac1b4a584970e6b518963b0d8dfd0551b2fde8ea
Taylor Musolf <tcmusolf@gmail.com>
2024-04-04 16:44:33 -0700

    work on fumble button logic and clean up natural fumbles json file

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { setupFumble } from './fumble';
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <h1>Generate a Fumble!</h1>
+    <div id="card">
+      <button id="fumble" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+setupFumble(document.querySelector('#fumble'), document.querySelector('#card'));
+
+// setupCounter(document.querySelector('#counter'))
